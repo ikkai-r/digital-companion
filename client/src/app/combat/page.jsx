e41904778dc2be7e88a1f59a90dd032d848cb5d1
ikkai-r <rm.sales92@gmail.com>
2024-04-05 18:26:53 +0800

    added: params to combat page

--- client/src/app/combat/page.jsx
+++ client/src/app/combat/page.jsx
@@ -1,22 +1,18 @@
+"use client"
+
 import React from 'react'
+import { useSearchParams } from 'next/navigation'
 
 export default function CombatPage() {
-  let predator = 1;
+  const searchParams = useSearchParams()
+  const predator = searchParams.get('ptor')
+  const prey = searchParams.get('prey')
+
 
   return (
     <section id="CombatPage" className='bg-background w-full min-h-screen text-text primary-text flex flex-col'>
       <div className='w-full flex flex-col text-center text-3xl accent-text justify-center mt-5'>
-        <p>Player {predator}</p>
-        <p className='mt-4 uppercase'>challenges</p>
-        <div className='flex gap-3 items-center justify-center w-full mt-4'>
-        <p>Player</p>
-        <select id="countries" class="bg-secondary text-text text-3xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
-              {predator === 1 ? null : <option value="1">1 ‎ ‎ </option>}
-              {predator === 2 ? null : <option value="2">2 ‎ ‎ </option>}
-              {predator === 3 ? null : <option value="3">3 ‎ ‎ </option>}
-              {predator === 4 ? null : <option value="4">4 ‎ ‎ </option>}
-            </select>
-        </div>
+        
       </div>
     </section>
   )
