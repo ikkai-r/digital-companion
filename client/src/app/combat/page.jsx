db71e061f8120349cf6a6ff31bd28eef9f781d59
ikkai-r <rm.sales92@gmail.com>
2024-04-09 22:17:26 +0800

    feat: added combat page todo: roll anim

--- client/src/app/combat/page.jsx
+++ client/src/app/combat/page.jsx
@@ -1,19 +1,92 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
+import { Modal, Button } from 'flowbite-react'
+import D20 from '@/components/D20'
 
 export default function CombatPage() {
   const searchParams = useSearchParams()
   const predator = searchParams.get('ptor')
   const prey = searchParams.get('prey')
 
+  const [predatorScore, setPredatorScore] = useState(null);
+  const [preyScore, setPreyScore] = useState(null);
+  const [rollPredator, setRollPredator] = useState(false);
+  const [rollPrey, setRollPrey] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+
+  const handleRollPredator = () => {
+    setRollPredator(true);
+    if (rollPrey) {
+
+      setShowResult(true);
+
+      // reset
+      setRollPredator(false);
+      setRollPrey(false);
+    }
+  };
+
+  const handleRollPrey = () => {
+    setRollPrey(true);
+    if (rollPredator) {
+      // show result
+      setShowResult(true);
+
+      // reset
+      setRollPredator(false);
+      setRollPrey(false);
+    }
+  };
+
 
   return (
     <section id="CombatPage" className='bg-background w-full min-h-screen text-text primary-text flex flex-col'>
-      <div className='w-full flex flex-col text-center text-3xl accent-text justify-center mt-5'>
-        
+      <div className='w-full flex flex-col text-3xl accent-text justify-center'>
+        <div className='w-full bg-accent3 p-10 h-[10em] flex flex-col justify-center items-center'>
+        <Button className='w-28 mb-10' style={{ backgroundColor: '#4056a1' }} onClick={handleRollPredator}>
+                {"Roll"}
+          </Button>
+          <D20
+            number='2'
+          />
+        </div>
+
+        <div className="bg-accent text-center py-2" style={{width: '100%'}}>
+          <div className='w-full text-center flex gap-5 items-center justify-center'>
+          <p className='text-lg'>P1</p>
+           <p className='text-lg'>P2</p>
+          </div>
+           <p className='text-4xl'>2 : 1</p>
+        </div>
+        <div className='w-full bg-secondary p-10 h-[10em] flex flex-col justify-center items-center'>
+          <D20
+            number='5'
+          />
+          <Button className='w-28 mt-16' style={{ backgroundColor: '#FB5A48' }} onClick={handleRollPrey}>
+                                {"Roll"}
+                            </Button>
+        </div>
       </div>
+
+      <Modal show={showResult} className='h-full ' size="md" onClose={() => setShowResult(false)} popup>
+                <Modal.Header className='bg-background' />
+                <Modal.Body className='bg-background'>
+                    <div className="text-center primary-text bg-background py-5 flex justify-center flex-col items-center gap-4">
+
+                        <p className="text-3xl accent-text uppercase text-accent">
+                            P1 has won!
+                        </p>
+
+                        <div className="flex justify-center gap-4 mt-4">
+                            <Button onClick={() => setShowResult(false)} className='w-28' style={{ backgroundColor: '#4056a1' }}>
+                                {"Okay!"}
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
     </section>
   )
 }
